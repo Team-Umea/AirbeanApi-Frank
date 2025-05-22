725dc8b2b858d6c79de11e98a96b55ae12cb21dd
Frank <Frankuik@hotmail.com>
2025-05-22 13:47:49 +0200

    sebbe wants to play

--- routes/order.js
+++ routes/order.js
@@ -1,14 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .send("hämta din order på ditt orderId som du fick när du beställde");
-});
-
-router.post("/", (req, res) => {});
-
-export default router;
+//skapa en order hämtar dina items från cart och lägger in det i order
+router.post("/");
 
 //make an order
+//min profil ska visa orderhistorik som visar tot summa och orderid samt datum
+router.get("/history/:userId");
+
+//visar din order status med förväntad leveranstid
+router.get("/status/:orderId");
+
+export default router;
